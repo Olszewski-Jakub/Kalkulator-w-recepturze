c26594135a675e7f80d2da69ee5ea6ed3551c682
Jakub Olszewski <j.olszewski05@gmail.com>
2023-06-02 15:38:39 +0200

    -Fragment Vitamin A+D3 was created and implemented. Vitamin A+D3 is fully functional now crashes were found, but still needs approval from tester according calculations -Deactivated Vitamin A v1 and switched to v2 after consulting with pharmacist,because it is easier to use. Same design is implemented to current elements(Vitamin A, Vitamin E, Vitamin A+D3) and will be implemented into future features of an applications -App passed an Robo test. Currently implemented features(Vitamin A, Vitamin E and Vitamin A+D3) are ready for user testing

--- app/src/main/java/com/jakubolszewski/kalkulatorwrecepturzedoz/fragments/VItaminAplusD3Fragment.kt
+++ app/src/main/java/com/jakubolszewski/kalkulatorwrecepturzedoz/fragments/VItaminAplusD3Fragment.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButtonToggleGroup
+import com.jakubolszewski.kalkulatorwrecepturzedoz.Adapters.VitaminAD3GridAdapter
+import com.jakubolszewski.kalkulatorwrecepturzedoz.Adapters.VitaminAD3GridModel
+import com.jakubolszewski.kalkulatorwrecepturzedoz.Adapters.VitaminAGridModel
 import com.jakubolszewski.kalkulatorwrecepturzedoz.R
+import com.jakubolszewski.kalkulatorwrecepturzedoz.calculations.VitaminAD3Calculations
+import com.jakubolszewski.kalkulatorwrecepturzedoz.database.DBHelper
+import com.jakubolszewski.kalkulatorwrecepturzedoz.database.Models.VitAD3Model
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private lateinit var dbHelper: DBHelper
+
 
 /**
  * A simple [Fragment] subclass.
@@ -18,43 +28,69 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class VItaminAplusD3Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var gridView: GridView
+    private lateinit var resultList: ArrayList<VitaminAD3GridModel>
+    private lateinit var calcButton: Button
+    private lateinit var editText: EditText
+    private lateinit var backImgView: ImageView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_v_itamin_aplus_d3, container, false)
-    }
+        val view: View = inflater.inflate(R.layout.fragment_v_itamin_aplus_d3, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment VItaminAplusD3Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            VItaminAplusD3Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        dbHelper = activity?.let { DBHelper(it) }!!
+        var vitAD3List: ArrayList<VitAD3Model> = ArrayList()
+        vitAD3List = dbHelper.getAllVitAD3()
+
+        calcButton = view.findViewById(R.id.button_calc)
+        editText = view.findViewById(R.id.editText_amount)
+        backImgView = view.findViewById(R.id.imageView_arrow)
+
+        gridView = view.findViewById(R.id.grid_vit_a_d3)
+        resultList = ArrayList()
+        val unitsGroup: MaterialButtonToggleGroup =
+            view.findViewById(R.id.toggle_button_group_units)
+        var unit: Int = -1
+
+        unitsGroup.addOnButtonCheckedListener { _, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.button_vit_a_d3_g -> unit = 0
+                    R.id.button_vit_a_d3_ml -> unit = 1
+                    R.id.button_vit_a_d3_krople -> unit = 2
                 }
             }
+        }
+
+        calcButton.setOnClickListener { _ ->
+            //Check if all fields are filled
+            if (editText.text.isNotBlank()) {
+                val amount: Double = editText.text.toString().toDouble()
+
+                Toast.makeText(context, unit.toString(), Toast.LENGTH_SHORT).show()
+                resultList.clear()
+                val Vitamins: Map<String, VitaminAD3GridModel> = VitaminAD3Calculations(
+                    units = unit,
+                    amount = amount,
+                    vitAD3List = vitAD3List
+                ).calculate()
+
+                Vitamins["Vit"]?.let {
+                    resultList.add(
+                        it
+                    )
+                }
+
+                val menuAdapter =
+                    context?.let { VitaminAD3GridAdapter(resultsList = resultList, context = it) }
+
+                gridView.adapter = menuAdapter
+            }
+        }
+
+        return view
     }
+
 }
